8c29c56a7cc6a6c19ba308e3a38654939568edbd
lleroc <llleroc@yahoo.es>
2024-07-31 20:39:17 -0500

    cambios

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace _02_Alumnos
 {
-    partial class Form1
+    partial class Form_Alumnos
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -46,30 +46,32 @@ namespace _02_Alumnos
             this.btn_Cancelar = new System.Windows.Forms.Button();
             this.btn_Eliminar = new System.Windows.Forms.Button();
             this.dtp_Fecha = new System.Windows.Forms.DateTimePicker();
+            this.btn_Modificar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lst_Alumnos
             // 
             this.lst_Alumnos.FormattingEnabled = true;
             this.lst_Alumnos.ItemHeight = 24;
-            this.lst_Alumnos.Location = new System.Drawing.Point(800, 52);
+            this.lst_Alumnos.Location = new System.Drawing.Point(591, 58);
             this.lst_Alumnos.Name = "lst_Alumnos";
-            this.lst_Alumnos.Size = new System.Drawing.Size(384, 460);
-            this.lst_Alumnos.TabIndex = 0;
+            this.lst_Alumnos.Size = new System.Drawing.Size(562, 460);
+            this.lst_Alumnos.TabIndex = 11;
             // 
             // btn_Agregar
             // 
-            this.btn_Agregar.Location = new System.Drawing.Point(359, 315);
+            this.btn_Agregar.Location = new System.Drawing.Point(19, 322);
             this.btn_Agregar.Name = "btn_Agregar";
             this.btn_Agregar.Size = new System.Drawing.Size(137, 62);
-            this.btn_Agregar.TabIndex = 1;
+            this.btn_Agregar.TabIndex = 7;
             this.btn_Agregar.Text = "Agregar";
             this.btn_Agregar.UseVisualStyleBackColor = true;
+            this.btn_Agregar.Click += new System.EventHandler(this.btn_Agregar_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(375, 52);
+            this.label1.Location = new System.Drawing.Point(230, 58);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(169, 24);
             this.label1.TabIndex = 2;
@@ -77,16 +79,16 @@ namespace _02_Alumnos
             // 
             // txt_Cedula
             // 
-            this.txt_Cedula.Location = new System.Drawing.Point(420, 94);
+            this.txt_Cedula.Location = new System.Drawing.Point(193, 100);
             this.txt_Cedula.MaxLength = 17;
             this.txt_Cedula.Name = "txt_Cedula";
             this.txt_Cedula.Size = new System.Drawing.Size(362, 29);
-            this.txt_Cedula.TabIndex = 3;
+            this.txt_Cedula.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(344, 94);
+            this.label2.Location = new System.Drawing.Point(117, 100);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(70, 24);
             this.label2.TabIndex = 4;
@@ -95,7 +97,7 @@ namespace _02_Alumnos
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(250, 269);
+            this.label3.Location = new System.Drawing.Point(23, 275);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(164, 24);
             this.label3.TabIndex = 5;
@@ -104,7 +106,7 @@ namespace _02_Alumnos
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(329, 234);
+            this.label4.Location = new System.Drawing.Point(102, 240);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(85, 24);
             this.label4.TabIndex = 6;
@@ -113,7 +115,7 @@ namespace _02_Alumnos
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(324, 199);
+            this.label5.Location = new System.Drawing.Point(97, 205);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(90, 24);
             this.label5.TabIndex = 7;
@@ -122,7 +124,7 @@ namespace _02_Alumnos
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(324, 164);
+            this.label6.Location = new System.Drawing.Point(97, 170);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(88, 24);
             this.label6.TabIndex = 8;
@@ -131,7 +133,7 @@ namespace _02_Alumnos
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(324, 129);
+            this.label7.Location = new System.Drawing.Point(97, 135);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(88, 24);
             this.label7.TabIndex = 9;
@@ -139,66 +141,79 @@ namespace _02_Alumnos
             // 
             // txt_Nombres
             // 
-            this.txt_Nombres.Location = new System.Drawing.Point(420, 129);
+            this.txt_Nombres.Location = new System.Drawing.Point(193, 135);
             this.txt_Nombres.MaxLength = 200;
             this.txt_Nombres.Name = "txt_Nombres";
             this.txt_Nombres.Size = new System.Drawing.Size(362, 29);
-            this.txt_Nombres.TabIndex = 10;
+            this.txt_Nombres.TabIndex = 2;
             // 
             // txt_Apellidos
             // 
-            this.txt_Apellidos.Location = new System.Drawing.Point(420, 164);
+            this.txt_Apellidos.Location = new System.Drawing.Point(193, 170);
             this.txt_Apellidos.MaxLength = 200;
             this.txt_Apellidos.Name = "txt_Apellidos";
             this.txt_Apellidos.Size = new System.Drawing.Size(362, 29);
-            this.txt_Apellidos.TabIndex = 11;
+            this.txt_Apellidos.TabIndex = 3;
             // 
             // txt_Direccion
             // 
-            this.txt_Direccion.Location = new System.Drawing.Point(420, 199);
+            this.txt_Direccion.Location = new System.Drawing.Point(193, 205);
             this.txt_Direccion.MaxLength = 200;
             this.txt_Direccion.Name = "txt_Direccion";
             this.txt_Direccion.Size = new System.Drawing.Size(362, 29);
-            this.txt_Direccion.TabIndex = 12;
+            this.txt_Direccion.TabIndex = 4;
             // 
             // txt_Telefono
             // 
-            this.txt_Telefono.Location = new System.Drawing.Point(420, 234);
+            this.txt_Telefono.Location = new System.Drawing.Point(193, 240);
             this.txt_Telefono.MaxLength = 200;
             this.txt_Telefono.Name = "txt_Telefono";
             this.txt_Telefono.Size = new System.Drawing.Size(362, 29);
-            this.txt_Telefono.TabIndex = 13;
+            this.txt_Telefono.TabIndex = 5;
             // 
             // btn_Cancelar
             // 
-            this.btn_Cancelar.Location = new System.Drawing.Point(502, 315);
+            this.btn_Cancelar.Location = new System.Drawing.Point(162, 322);
             this.btn_Cancelar.Name = "btn_Cancelar";
             this.btn_Cancelar.Size = new System.Drawing.Size(137, 62);
-            this.btn_Cancelar.TabIndex = 15;
+            this.btn_Cancelar.TabIndex = 8;
             this.btn_Cancelar.Text = "Cancelar";
             this.btn_Cancelar.UseVisualStyleBackColor = true;
+            this.btn_Cancelar.Click += new System.EventHandler(this.btn_Cancelar_Click);
             // 
             // btn_Eliminar
             // 
-            this.btn_Eliminar.Location = new System.Drawing.Point(645, 315);
+            this.btn_Eliminar.Location = new System.Drawing.Point(448, 322);
             this.btn_Eliminar.Name = "btn_Eliminar";
             this.btn_Eliminar.Size = new System.Drawing.Size(137, 62);
-            this.btn_Eliminar.TabIndex = 16;
+            this.btn_Eliminar.TabIndex = 10;
             this.btn_Eliminar.Text = "Eliminar";
             this.btn_Eliminar.UseVisualStyleBackColor = true;
+            this.btn_Eliminar.Click += new System.EventHandler(this.btn_Eliminar_Click);
             // 
             // dtp_Fecha
             // 
-            this.dtp_Fecha.Location = new System.Drawing.Point(420, 269);
+            this.dtp_Fecha.Location = new System.Drawing.Point(193, 275);
             this.dtp_Fecha.Name = "dtp_Fecha";
             this.dtp_Fecha.Size = new System.Drawing.Size(362, 29);
-            this.dtp_Fecha.TabIndex = 17;
+            this.dtp_Fecha.TabIndex = 6;
             // 
-            // Form1
+            // btn_Modificar
+            // 
+            this.btn_Modificar.Location = new System.Drawing.Point(305, 322);
+            this.btn_Modificar.Name = "btn_Modificar";
+            this.btn_Modificar.Size = new System.Drawing.Size(137, 62);
+            this.btn_Modificar.TabIndex = 9;
+            this.btn_Modificar.Text = "Modificar";
+            this.btn_Modificar.UseVisualStyleBackColor = true;
+            this.btn_Modificar.Click += new System.EventHandler(this.btn_Modificar_Click);
+            // 
+            // Form_Alumnos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1459, 1061);
+            this.Controls.Add(this.btn_Modificar);
             this.Controls.Add(this.dtp_Fecha);
             this.Controls.Add(this.btn_Eliminar);
             this.Controls.Add(this.btn_Cancelar);
@@ -217,10 +232,11 @@ namespace _02_Alumnos
             this.Controls.Add(this.btn_Agregar);
             this.Controls.Add(this.lst_Alumnos);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.Name = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form_Alumnos";
             this.Text = "Alumnos";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -245,6 +261,7 @@ namespace _02_Alumnos
         private System.Windows.Forms.Button btn_Cancelar;
         private System.Windows.Forms.Button btn_Eliminar;
         private System.Windows.Forms.DateTimePicker dtp_Fecha;
+        private System.Windows.Forms.Button btn_Modificar;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,97 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace _02_Alumnos
 {
-    public partial class Form1 : Form
+    public partial class Form_Alumnos : Form
     {
-        public Form1()
+        bool estado = false;
+        int posicion = -1;
+        public Form_Alumnos()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Agregar_Click(object sender, EventArgs e)
+        {
+            
+            if (txt_Cedula.Text != "" && txt_Nombres.Text != "" && txt_Apellidos.Text != "" && txt_Direccion.Text != "" && txt_Telefono.Text != "")
+            {
+               if (estado == true)
+                {
+                    lst_Alumnos.Items[posicion] = txt_Cedula.Text +" || "+ txt_Nombres.Text + " || " + txt_Apellidos.Text + " || " + txt_Direccion.Text + " || " + txt_Telefono.Text + " || " + dtp_Fecha.Text;
+                    estado = false;
+                    posicion = -1;
+                    LimpiarCajas();
+                }
+                else
+                {               
+                    lst_Alumnos.Items.Add(txt_Cedula.Text + " || " + txt_Nombres.Text + " || " + txt_Apellidos.Text + " || " + txt_Direccion.Text + " || " + txt_Telefono.Text + " || " + dtp_Fecha.Text);
+                    LimpiarCajas();
+                    estado = false;
+                }
+            }
+            else { MessageBox.Show("Complete las casillas para guardar la informacion"); }
+
+            
+        }
+
+        public void LimpiarCajas() {
+            txt_Apellidos.Text = "";
+            txt_Cedula.Text = "";
+            txt_Direccion.Text = "";
+            txt_Nombres.Text = "";
+            txt_Telefono.Text = "";         
+        }
+        private void btn_Eliminar_Click(object sender, EventArgs e)
+        {
+            if (lst_Alumnos.SelectedIndex == -1)
+            {
+                MessageBox.Show("Companiero, seleccione un item de la lista");
+                return;
+            }
+                DialogResult dialogResult = 
+                    MessageBox.Show("Esta seguro que desea eliminar al alumno!!!", "Alumnos", 
+                    MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    lst_Alumnos.Items.RemoveAt(lst_Alumnos.SelectedIndex);
+                    MessageBox.Show("Se elimino con exito");
+                }
+                else {
+                    MessageBox.Show("El usuario cancelo la operacion");
+                }
+        }
+
+        private void btn_Modificar_Click(object sender, EventArgs e)
+        {
+            if (lst_Alumnos.SelectedIndex != -1)
+            {
+                string seleccion = lst_Alumnos.SelectedItem.ToString();
+                string[] partesdelAlumnos = seleccion.Split(new string[] { "||" }, StringSplitOptions.None);
+                txt_Cedula.Text = partesdelAlumnos[0];
+                txt_Nombres.Text = partesdelAlumnos[1];
+                txt_Apellidos.Text = partesdelAlumnos[2];
+                txt_Direccion.Text =  partesdelAlumnos[3];
+                txt_Telefono.Text = partesdelAlumnos[4];
+                dtp_Fecha.Text = partesdelAlumnos[5];
+                estado = true;
+                posicion = lst_Alumnos.SelectedIndex;
+            }
+            else
+            {
+                MessageBox.Show("Companiero, seleccione un item de la lista");
+            }
+        }
+
+        private void btn_Cancelar_Click(object sender, EventArgs e)
+        {
+            LimpiarCajas();
         }
     }
 }
